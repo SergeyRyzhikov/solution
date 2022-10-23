2cf89cf07f8a572c0d2d39af879273beddf09e2f
SergeyRyzhikov <sergey.p.ryzhikov@gmail.com>
2022-10-23 21:00:59 +0300

    fix

--- src/com/javarush/test/solution.java
+++ src/com/javarush/test/solution.java
@@ -1,23 +1,9 @@
 package com.javarush.test;
 
 public class solution {
-    public static String bug = "🐛";
-    public static String robot = "🤖";
-    public static String land = "🌍";
-    public static String death = "💀";
-    public static String hole = " ";
 
 
     public static void main(String[] args) {
-        //напишите тут ваш код
-        int i, j;
-        for (i=0;i<10;i++)
-        {
-            for (j=0;j<10;j++)
-            {
-                System.out.print(land);
-            }
-            System.out.println();
-        }
+
     }
 }
