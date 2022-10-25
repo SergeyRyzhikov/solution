6926ea63fd7c15985889fe88c618580f40e8555d
SergeyRyzhikov <sergey.p.ryzhikov@gmail.com>
2022-10-25 22:19:29 +0300

    array, search min

--- src/com/javarush/test/solution.java
+++ src/com/javarush/test/solution.java
@@ -2,25 +2,26 @@ package com.javarush.test;
 
 import java.util.Scanner;
 public class solution {
-        public static void main(String[] args) {
+        public static int [] array;
+
+    public static void main(String[] args) {
         //напишите тут ваш код
         Scanner console = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
         int N = console.nextInt();
-        int[] array;
-        if (N>0) {
-            array = new int[N];
-            for (int i=0; i<N; i++) {
-                array[i] = console.nextInt();
+        array = new int[N];
+        System.out.println("Заполните массив числами: ");
+        for (int i = 0; i < N; i++) {
+            System.out.print("Введите элемент № "+(i+1) + ": ");
+            array[i] = console.nextInt();
+        }
+        int min;
+        min = array[0];
+            for (int i=1; i<N; i++) {
+                if (array[i]<min)
+                    min = array[i];
             }
-            if (N % 2 == 0) {
-                for (int i = N-1; i >= 0; i--) {
-                    System.out.println(array[i]);
-                }
-            } else {
-                for (int i = 0; i < N; i++) {
-                    System.out.println(array[i]);
-                }
-            }
+        System.out.print("Минимальный элемент: " + min);
+
         }
     }
-}
